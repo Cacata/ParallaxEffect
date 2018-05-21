c2cc638eefc998329eb5a6fd7e7d6c0764968b0c
Mr. Cacata <armando.m.dorville@gmail.com>
2018-05-21 00:56:46 -0400

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Termspolicy from '../src/components/terms_policy/termspolicy';
+
+class App extends Component {
+  render() {
+    return (
+      <Termspolicy />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/terms_policy/termspolicy.jsx
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import './termspolicy.css';
+
+class termspolicy extends Component
+{
+    render(){
+        return(
+            <header>
+                <h3 className="title">
+                    Este es el titulo de las mejores politicas de tu vida
+                </h3>
+                <p className="paragraph">
+                    asdkjndlfglinerjnfg sdflgnjdflkg dsf;gk;lkgf.
+                    lakjdshfngdjanf jhfcv sdjfbsknjdg sdhnfsd sdngfsdkjg
+                    ksdfhgljudnf gjnsdhasi hugfpihjap oisjfd gsoijsdf hgihjsd hjgfoias.
+                    ksd dfsadf asdfibasdf asdjfnasdf asdljfnasdf asdjfnasdfj asdjhfasd
+                    kjsandf asdljk werjtng zxchvblwer a;sdoiqwfnb asy asdijfasdf.
+                </p>
+                <p className="paragraph">
+                    asdkjndlfglinerjnfg sdflgnjdflkg dsf;gk;lkgf.
+                    lakjdshfngdjanf jhfcv sdjfbsknjdg sdhnfsd sdngfsdkjg
+                    ksdfhgljudnf gjnsdhasi hugfpihjap oisjfd gsoijsdf hgihjsd hjgfoias.
+                    ksd dfsadf asdfibasdf asdjfnasdf asdljfnasdf asdjfnasdfj asdjhfasd
+                    kjsandf asdljk werjtng zxchvblwer a;sdoiqwfnb asy asdijfasdf.
+                </p>
+                <p className="paragraph">
+                    asdkjndlfglinerjnfg sdflgnjdflkg dsf;gk;lkgf.
+                    lakjdshfngdjanf jhfcv sdjfbsknjdg sdhnfsd sdngfsdkjg
+                    ksdfhgljudnf gjnsdhasi hugfpihjap oisjfd gsoijsdf hgihjsd hjgfoias.
+                    ksd dfsadf asdfibasdf asdjfnasdf asdljfnasdf asdjfnasdfj asdjhfasd
+                    kjsandf asdljk werjtng zxchvblwer a;sdoiqwfnb asy asdijfasdf.
+                </p>
+                <h3 className="title">
+                    Este es el titulo de las mejores politicas de tu vida
+                </h3>
+                <p className="paragraph">
+                    asdkjndlfglinerjnfg sdflgnjdflkg dsf;gk;lkgf.
+                    lakjdshfngdjanf jhfcv sdjfbsknjdg sdhnfsd sdngfsdkjg
+                    ksdfhgljudnf gjnsdhasi hugfpihjap oisjfd gsoijsdf hgihjsd hjgfoias.
+                    ksd dfsadf asdfibasdf asdjfnasdf asdljfnasdf asdjfnasdfj asdjhfasd
+                    kjsandf asdljk werjtng zxchvblwer a;sdoiqwfnb asy asdijfasdf.
+                </p>
+                <p className="paragraph">
+                    asdkjndlfglinerjnfg sdflgnjdflkg dsf;gk;lkgf.
+                    lakjdshfngdjanf jhfcv sdjfbsknjdg sdhnfsd sdngfsdkjg
+                    ksdfhgljudnf gjnsdhasi hugfpihjap oisjfd gsoijsdf hgihjsd hjgfoias.
+                    ksd dfsadf asdfibasdf asdjfnasdf asdljfnasdf asdjfnasdfj asdjhfasd
+                    kjsandf asdljk werjtng zxchvblwer a;sdoiqwfnb asy asdijfasdf.
+                </p>
+                <p className="paragraph">
+                    asdkjndlfglinerjnfg sdflgnjdflkg dsf;gk;lkgf.
+                    lakjdshfngdjanf jhfcv sdjfbsknjdg sdhnfsd sdngfsdkjg
+                    ksdfhgljudnf gjnsdhasi hugfpihjap oisjfd gsoijsdf hgihjsd hjgfoias.
+                    ksd dfsadf asdfibasdf asdjfnasdf asdljfnasdf asdjfnasdfj asdjhfasd
+                    kjsandf asdljk werjtng zxchvblwer a;sdoiqwfnb asy asdijfasdf.
+                </p>
+            </header>
+        );
+    }
+}
+
+export default termspolicy;
